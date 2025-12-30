b9bfb49a2a4fc4cbdde7805c4b40416c0a9a20a8
karthideva25 <77924615+karthideva25@users.noreply.github.com>
2025-12-30 13:28:33 +0530

    Update Segment Tree.cpp

--- Segment Tree.cpp
+++ Segment Tree.cpp
@@ -1,3 +1,96 @@
+class SegmentTree{
+    vector<int> seg;
+    int n;
+public:
+    SegmentTree(){
+        
+    }
+    SegmentTree(vector<int>& arr){
+        n = arr.size();
+        seg.resize(4*n);
+        build(0,0,n-1,arr);
+    }
+    int rangeQuery(int l, int r){
+        return query(0,0,n-1,l,r);
+    }
+    void pointUpdates(int pos, int val){
+        update(0,0,n-1,pos,val);
+    }
+private:
+    void build(int ind, int low, int high, vector<int>& arr){
+        if(low == high){
+            seg[ind] = arr[low];
+            return;
+        }
+        int mid = (low + high) / 2;
+        build(2*ind+1, low, mid, arr);
+        build(2*ind+2, mid+1, high, arr);
+        seg[ind] = seg[2*ind+1] + seg[2*ind+2];
+    }
+    int query(int ind, int low, int high, int l, int r){
+        // no overlap low high l r || l r low high
+        if(high < l or r < low){
+            return 0;
+        }
+        // complete overlap l low high r
+        else if(l <= low and high <= r){
+            return seg[ind];
+        }
+        // partial overlap
+        int mid = (low + high) / 2;
+        return query(2*ind+1, low, mid, l, r)
+        + query(2*ind+2, mid+1, high, l, r);
+    }
+    void update(int ind, int low, int high, int pos, int val){
+        // no overlap low high pos || pos low high
+        if(pos > high or pos < low){
+            return;
+        }
+        // complete overlap low pos high
+        if(low == pos and high == pos){
+            seg[ind] = val;
+            return;
+        }
+        // partial overlap case
+        int mid = (low + high) >> 1;
+        update(2*ind+1, low, mid, pos, val);
+        update(2*ind+2, mid+1, high, pos, val);
+        seg[ind] = seg[2*ind+1] + seg[2*ind+2];
+    }
+    
+};
+class NumArray {
+public:
+    SegmentTree seg;
+    NumArray(vector<int>& nums) {
+        SegmentTree tree(nums);
+        this->seg = tree;
+    }
+    
+    void update(int index, int val) {
+        seg.pointUpdates(index,val);
+    }
+    
+    int sumRange(int left, int right) {
+        return seg.rangeQuery(left,right);
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
