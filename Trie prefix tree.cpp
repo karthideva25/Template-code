0a9317112b873a2e33b7d67dd9cb6f3b763e8af0
karthideva25 <77924615+karthideva25@users.noreply.github.com>
2026-01-02 17:12:40 +0530

    Create Trie prefix tree.cpp

---
+++ Trie prefix tree.cpp
@@ -0,0 +1,53 @@
+class Trie{
+    struct TrieNode{
+        TrieNode* children[26];
+        bool isEnd;
+        
+        TrieNode(){
+            isEnd = false;
+            for(int i=0;i<26;i++){
+                children[i] = nullptr;
+            }
+        }
+    };
+    TrieNode* root;
+public:
+    Trie(){
+        trie = new TrieNode();
+    }
+    void insert(string word){
+        TrieNode* node = root;
+        for(char ch: word){
+            int index = ch - 'a';
+            if(node->children[index] == nullptr){
+                node->children[index] = new TrieNode();
+            }
+            node = node->children[index];
+        }
+        node->isEnd = true;
+    }
+    
+    bool search(string word){
+        TrieNode* node = root;
+        for(char ch: word){
+            int index = ch - 'a';
+            if(node->children[index] == nullptr){
+                return false;
+            }
+            node = node->children[index];
+        }
+        return node->isEnd;
+    }
+    
+    bool isStartsWith(string word){
+        TrieNode* node = root;
+        for(char ch: word){
+            int index = ch - 'a';
+            if(node->children[index] == nullptr){
+                return false;
+            }
+            node = node->children[index];
+        }
+        return true;
+    }
+};
