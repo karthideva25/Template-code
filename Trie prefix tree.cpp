afcf716e678f1ce5fdd8a8e956fef346a8c66c74
karthideva25 <77924615+karthideva25@users.noreply.github.com>
2026-01-02 17:17:40 +0530

    Update Trie prefix tree.cpp

--- Trie prefix tree.cpp
+++ Trie prefix tree.cpp
@@ -51,3 +51,60 @@ public:
         return true;
     }
 };
+
+
+
+class Trie {
+    struct TrieNode{
+        TrieNode* children[26];
+        bool isEnd;
+        
+        TrieNode(){
+            for(int i=0;i<26;i++){
+                children[i] = nullptr;
+            }
+        }
+    };
+    TrieNode* root;
+  public:
+
+    Trie() {
+        root = new TrieNode();
+    }
+
+    void insert(string &word) {
+        TrieNode* node = root;
+        for(char ch : word){
+            int index = ch - 'a';
+            if(node->children[index] == nullptr){
+                node->children[index] = new TrieNode();
+            }
+            node = node->children[index];
+        }
+        node->isEnd = true;
+    }
+
+    bool search(string &word) {
+        TrieNode* node = root;
+        for(char ch : word){
+            int index = ch - 'a';
+            if(node->children[index] == nullptr){
+                return false;
+            }
+            node = node->children[index];
+        }
+        return node->isEnd;
+    }
+
+    bool isPrefix(string &word) {
+        TrieNode* node = root;
+        for(char ch : word){
+            int index = ch - 'a';
+            if(node->children[index] == nullptr){
+                return false;
+            }
+            node = node->children[index];
+        }
+        return true;
+    }
+};
